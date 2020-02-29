2a1db77a5a3bcc1958b932c12dfb407951b50cf8
panchorc1 <panchorc1@gmai.com>
2020-02-28 21:17:29 -0600

    Iniciando proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
